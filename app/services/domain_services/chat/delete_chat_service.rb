37948325399eb75d8b3807f1ecbf69410bb8e039
Aram <aramhrptn@hotmail.com>
2023-11-15 20:44:48 +0400

    added chat delete

---
+++ app/services/domain_services/chat/delete_chat_service.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module DomainServices
+  module Chat
+    class DeleteChatService < ApplicationService
+      attr_reader :user, :chat
+
+      def initialize(user, chat)
+        super()
+        @user = user
+        @chat = chat
+      end
+
+      def call
+        raise DomainErrors::Chat::ChatOwnerError if chat.owner != user
+
+        chat.destroy!
+
+        chat
+      end
+    end
+  end
+end
