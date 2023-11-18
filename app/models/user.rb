2842e7cb11e69041f77de1d0b9607f63782cabcb
Aram <aramhrptn@hotmail.com>
2023-11-18 05:45:34 +0400

    user model refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,16 +14,15 @@ class User < ApplicationRecord
   validates_uniqueness_of :email, message: I18n.t('user.email_already_taken')
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP,
                               message: I18n.t('user.not_a_valid_email') }
-  validates :password, format: { with: /\d/, message: I18n.t('user.password.number_missing') }, if: lambda {
-                                                                                                      password.present?
-                                                                                                    }
-  validates :password, format: { with: /[A-Z]/, message: I18n.t('user.password.uppercase_missing') }, if: lambda {
-                                                                                                            password.present?
-                                                                                                          }
-  validates :password, format: { with: /[a-z]/, message: I18n.t('user.password.lowercase_missing') }, if: lambda {
-                                                                                                            password.present?
-                                                                                                          }
-  validates :password, format: { with: /\W/, message: I18n.t('user.password.special_character_missing') }, if: lambda {
-                                                                                                                 password.present?
-                                                                                                               }
+  validates :password, format: { with: /\d/, message: I18n.t('user.password.number_missing') }, if: :password_present?
+  validates :password, format: { with: /[A-Z]/, message: I18n.t('user.password.uppercase_missing') },
+                       if: :password_present?
+  validates :password, format: { with: /[a-z]/, message: I18n.t('user.password.lowercase_missing') },
+                       if: :password_present?
+  validates :password, format: { with: /\W/, message: I18n.t('user.password.special_character_missing') },
+                       if: :password_present?
+
+  private
+
+  def password_present? = password.present?
 end
