07075ded31dae3158abb00fcbd559b1626ea17ab
Aram <aramhrptn@hotmail.com>
2023-11-19 07:00:55 +0400

    spec refactor

--- app/services/domain_services/user/add_user_datum_service.rb
+++ app/services/domain_services/user/add_user_datum_service.rb
@@ -14,7 +14,6 @@ module DomainServices
       end
 
       def call
-        # user.user_setting.update(first_name:, last_name:, date_of_birth:)
         user.user_setting.first_name = first_name unless first_name.nil? || first_name.blank?
         user.user_setting.last_name = last_name unless last_name.nil? || last_name.blank?
         user.user_setting.date_of_birth = date_of_birth unless date_of_birth.nil? || date_of_birth.blank?
