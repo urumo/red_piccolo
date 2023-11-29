969e05855e3ddc0eaad36ec31a2ff958ec7cc835
Aram <aramhrptn@hotmail.com>
2023-11-29 21:29:36 +0400

    fixed linting error

--- app/graphql/types/node_type.rb
+++ app/graphql/types/node_type.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 module Types
+  # rubocop:disable GraphQL/ObjectDescription
   module NodeType
     include Types::BaseInterface
     # Add the `id` field
     include GraphQL::Types::Relay::NodeBehaviors
   end
+  # rubocop:enable GraphQL/ObjectDescription
 end
