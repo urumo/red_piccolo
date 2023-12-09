7a967e6f412b8bff8bd0fb4309ed97f6b7aee7dc
Aram <aramhrptn@hotmail.com>
2023-12-09 21:15:33 +0400

    modified the application error class

--- app/errors/application_error.rb
+++ app/errors/application_error.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationError < StandardError
-  def initialize(**args, &block)
+  def initialize(msg = nil)
     raise ArgumentError, 'Change the error message' if message == 'CHANGE ME'
 
     super
