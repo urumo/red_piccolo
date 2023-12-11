aec112e20b5a29bd03951fc6a4ea70097919df19
urumo <aramhrptn@hotmail.com>
2023-12-11 14:59:23 +0700

    added rubocop profilers and fixed some front end logic (#18)
    > ## Summary by CodeRabbit
    > * **New Features**
    >   
    >   * Enhanced navigation experience with an updated assignment of the home page redirection.
    > * **Refactor**
    >   
    >   * Improved error handling with a more specific function signature and a new default export for better error management.
    >   * Streamlined the Home View by removing unused authorization logic.
    > * **Chores**
    >   
    >   * Added development tools to assist with performance profiling.
    
    > ## Walkthrough
    > The recent updates include adding performance profiling tools to the development stack, refining the navigation behavior in a Vue component, enhancing TypeScript type safety for retry options, and restructuring error handling for better type assertion. Additionally, there's a simplification in the home view by removing unused authorization logic.
    > 
    > ## Changes
    > File Path	Summary
    > `Gemfile`	Added `memory_profiler` and `stackprof` gems to the development and test groups.
    > `app/javascript/components/.../NavBar.vue`	Updated `window.location` assignment to use `href` property.
    > `app/javascript/entrypoints/.../application.ts`	Imported `RetryExchangeOptions` type and updated `retryOptions` with type annotation.
    > `app/javascript/errorHandler.ts`	Extracted `handle` function and updated default export with type assertions.
    > `app/javascript/pages/.../HomeView.vue`	Removed unused `auth` variable and related conditional rendering.
    > > 🐇✨
    > > In the code where gems align,
    > > A rabbit hops through the design.
    > > With types and states so neatly spun,
    > > Our digital burrow's changes are done. 🌟🌿
    >

--- app/javascript/pages/HomeView.vue
+++ app/javascript/pages/HomeView.vue
@@ -3,7 +3,6 @@ import { useCounterStore } from '@/stores/counter';
 import { useAuthorizationStore } from '@/stores/useAuthorization';
 
 const counter = useCounterStore();
-const auth = useAuthorizationStore();
 </script>
 
 <template>
@@ -12,7 +11,6 @@ const auth = useAuthorizationStore();
     <h2>{{ counter.count }}</h2>
     <h2>{{ counter.doubleCount }}</h2>
     <v-btn @click="counter.increment">Increment</v-btn>
-    <v-btn v-if="auth.userIsSet" @click="auth.authorize">Authorize</v-btn>
   </main>
 </template>
 <style scoped></style>
