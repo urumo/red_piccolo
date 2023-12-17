8d6196ba2b3e0e152a423e2dfb59dc1bceca65d6
Aram <aramhrptn@hotmail.com>
2023-12-17 04:31:19 +0400

    refactor

--- config/initializers/sneakers.rb
+++ config/initializers/sneakers.rb
@@ -9,18 +9,20 @@ AMPQ_URL = ENV.fetch('RABBITMQ_URL') do |key|
   'amqp://guest:guest@localhost:5672'
 end
 
-Sneakers.configure(metrics: Sneakers::Metrics::LoggingMetrics.new)
-Sneakers.logger.level = Rails.env.production? ? Logger::WARN : Logger::DEBUG
 Sneakers.logger = Rails.logger
-Sneakers.configure heartbeat: 30,
-                   amqp: AMPQ_URL,
-                   vhost: ENV.fetch('RABBITMQ_VHOST', '/'),
-                   workers: 4,
-                   threads: 4,
-                   pid_path: 'tmp/pids/sneakers.pid',
-                   env: Rails.env,
-                   hooks: {
-                     before_fork: -> { ActiveRecord::Base.connection_pool.disconnect! },
-                     after_fork: -> { ActiveRecord::Base.establish_connection }
-                   }
+Sneakers.logger.level = Rails.env.production? ? Logger::WARN : Logger::DEBUG
+Sneakers.configure(
+  heartbeat: 30,
+  amqp: AMPQ_URL,
+  vhost: ENV.fetch('RABBITMQ_VHOST', '/'),
+  workers: 4,
+  threads: 4,
+  pid_path: 'tmp/pids/sneakers.pid',
+  env: Rails.env,
+  metrics: Sneakers::Metrics::LoggingMetrics.new,
+  hooks: {
+    before_fork: -> { ActiveRecord::Base.connection_pool.disconnect! },
+    after_fork: -> { ActiveRecord::Base.establish_connection }
+  }
+)
 Sneakers.logger.info "Sneakers configured for #{Rails.env} environment"
